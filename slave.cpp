0252f26b386a4697297911b16ac3a7a3a6faae28
samyoahri <samyoahri@gmail.com>
2023-12-17 01:19:41 +0900

    Update slave cpp especially sending code

--- slave.cpp
+++ slave.cpp
@@ -1,69 +1,54 @@
+#include <Servo.h>
 #include <ArduinoJson.h>
 
-int steer = 7;
-int throttle = 8;
-int left = 2;
-int right = 3;
-int center = 4;
+const int steer = 7;
+const int throttle = 8;
+const int left = 3;
+const int right = 2;
+const int center = 4;
 int c_val = 0;
 int l_val = 0;
 int r_val = 0;
-
 unsigned long steer_duration;
 unsigned long steer_overall_duration;
 unsigned long throttle_duration;
 unsigned long throttle_overall_duration;
 
-
 void setup(){
-    Serial.begin(9600);
-    pinMode(steer,INPUT);
-    pinMode(throttle, INPUT);
-    pinMode(left, INPUT);
-    pinMode(right, INPUT);
-    pinMode(center, INPUT);
+  Serial.begin(115200);
+  pinMode(left, INPUT);
+  pinMode(right, INPUT);
+  pinMode(center, INPUT);
 }
-
 
 void loop() {
-    steer_duration = pulseIn(steer,HIGH);
-    steer_overall_duration = pulseIn(steer,LOW);
-    steer_overall_duration += steer_duration;
+  int c_val = digitalRead(center);
+  int l_val = digitalRead(left);
+  int r_val = digitalRead(right);
 
-    throttle_duration = pulseIn(steer,HIGH);
-    throttle_overall_duration = pulseIn(steer,LOW);
-    throttle_overall_duration += throttle_duration;
+  if (c_val == LOW) {
+    Serial.print("Center:0");
+    Serial.print(",");
+  }
+  else {
+    Serial.print("Center:1");
+    Serial.print(",");
+  }
 
-    Serial.print("steer_duration : ");
-    Serial.print(steer_duration);
-    Serial.print("/");
-
-    Serial.print(steer_overall_duration);
-    Serial.print(" throttle_duration : ");
-    Serial.print(throttle_duration);
-    Serial.print("/");
-    Serial.printin(throttle_overall_duration);
-
-    if(c_val == LOW){
-        Serial.printin(1);
+  if (l_val == LOW) {
+    Serial.print("left:0");
+    Serial.print(",");
     }
-    else{
-        Serial.printin(0);
+    else {
+    Serial.print("left:1");
+    Serial.print(",");
     }
 
-    if(l_val == LOW){
-        Serial.printin(1);
-    }
-    else{
-        Serial.printin(0);
-    }
-
-    if(r_val == LOW){
-        Serial.printin(1);
-    }
-    else{
-        Serial.printin(0);
-    }
-    delay(500);
+  if (r_val == LOW) {
+    Serial.println("Right:0");
+  }
+  else {
+    Serial.println("Right:1");
+  }
+  delay(10);
 }
-
